02f0e5705c9ab9e3e8ba35250b574f78ff1e7203
krishnamuddasani <krishnamuddasani@granular.ag>
2022-10-03 11:47:11 -0500

    Update Q1_Two_Sum.py

--- Easy_Python/Q1_Two_Sum.py
+++ Easy_Python/Q1_Two_Sum.py
@@ -30,12 +30,13 @@ from typing import List
 class Solution:
     @staticmethod
     def two_sum(nums: List[int], target: int) -> List[int]:
-        for i in range(1, len(nums)):
-            if nums[i-1] + nums[i] == target:
-                return [i-1, i]
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
 
 
 print(Solution.two_sum([2, 7, 11, 15], 9))
 print(Solution.two_sum([3, 2, 4], 6))
 print(Solution.two_sum([3, 3], 6))
-
+print(Solution.two_sum([3, 2, 3], 6))
